5a784a70fd0d3a4b4458fa368f2cb9744abee435
Suresh Pawar <sureshpawar270@gmail.com>
2024-06-12 16:43:41 +0530

    Work on Html and Css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,24 @@
 const cl = console.log;
 
-const list = document.getElementById('list');
-const main = document.getElementById('para');
-
-const angularLi = document.createElement('li');
-angularLi.innerHTML = 'Anglular';
-angularLi.className = 'list-group-item'
-// cl(angularLi);
-//append method add the elemen at last
-list.append(angularLi);
-// ############ Textnode ######################
-//to create a li
-const tsli = document.createElement('li');
-//add className on li
-tsli.className = 'list-group-item'; 
-//create a textnode
-const tsliText = document.createTextNode('TypeScript')
-//append tsliText in tsli and list prepend the tsli
-tsli.append(tsliText );
-list.prepend(tsli)
-
-const cssLi = document.createElement('li');
-cssLi.className = 'list-group-item';
-const cssText = document.createTextNode("CSS");
-cssLi.prepend(cssText);
-list.append(cssLi)
-
-// ########################## add and remove element     ###########################################
-
-const oldPara = document.getElementById('para')
-// let removeELe = main.remove();
-const newPara = document.createElement('p');
-newPara.className = 'alert alert-info mt-4'
-newPara.innerHTML = 'Angular Is A Famouse FrameWork';
-
-oldPara.parentNode.replaceChild(newPara,oldPara);
+const addMovieForm = document.getElementById('addMovieForm');
+const addMovieBtn = document.getElementById('addMovieBtn');
+const backDrop = document.getElementById('backDrop');
+const ourModel = document.getElementById('ourModel');
+const closeTab = [...document.querySelectorAll('.closeTab')];
+cl(closeTab);
 
 
 
 
-const newSkillList = document.querySelector('#skillList li:last-child');
-cl(newSkillList);
-const javaScript = document.createElement('li');
-javaScript.className = 'list-group-item';
-javaScript.innerHTML = 'JavaScript';
-newSkillList.parentNode.insertBefore(javaScript,newSkillList);//(newAddList,lastSelectesLIst)
+
+
+const onAddMovieData = ((eve)=>{
+    eve.preventDefault();
+    cl('submit');
+    backDrop.classList.add('d-none')
+    ourModel.classList.add('d-none')
+
+})
 
 
 
@@ -54,4 +28,4 @@ newSkillList.parentNode.insertBefore(javaScript,newSkillList);//(newAddList,last
 
 
 
-
+addMovieForm.addEventListener('submit',onAddMovieData)
